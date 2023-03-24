762d2877049835d2949206244c713080592cf2ed
Fredisma0169 <97203352+Fredisma0169@users.noreply.github.com>
2023-03-24 12:32:36 +0100

    Initial Commit

---
+++ Fitness/FitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessApp.swift
+//  Fitness
+//
+//  Created by Fredisma OUEDRAOGO on 24/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct FitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
